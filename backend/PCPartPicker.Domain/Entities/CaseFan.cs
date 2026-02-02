c3997cc3235b5ad41870d03c45e85382d0df4f44
ius_dv <iusdevisser@gmail.com>
2026-02-02 23:12:01 +0100

    added domain for casefan, build slot and enum

---
+++ backend/PCPartPicker.Domain/Entities/CaseFan.cs
@@ -0,0 +1,11 @@
+using PCPartPicker.Domain.Enums;
+
+namespace PCPartPicker.Domain.Entities;
+
+public class CaseFan : Part
+{
+    public CaseFan()
+    {
+        Category = PartCategory.CaseFan;
+    }
+}
